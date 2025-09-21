15f191a5bbcbe1a3be7b3128cc689eaa3af6918b
juanmunizy <164834167+JuanMunizy@users.noreply.github.com>
2025-09-20 21:47:20 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPasswordField; // [00:18:36]
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,29 +22,28 @@ public class Main {
         campoUsuario.setBounds(50, 80, 150, 30);
         janela.add(campoUsuario);
 
-        JLabel labelSenha = new JLabel("Senha:"); // [00:19:36]
-        labelSenha.setBounds(50, 150, 100, 30); // [00:19:58]
-        janela.add(labelSenha); // [00:20:30]
+        JLabel labelSenha = new JLabel("Senha:");
+        labelSenha.setBounds(50, 150, 100, 30);
+        janela.add(labelSenha);
 
-        JPasswordField campoSenha = new JPasswordField(); // [00:18:36]
-        campoSenha.setBounds(50, 180, 150, 30); // [00:18:58]
-        janela.add(campoSenha); // [00:19:18]
+        JPasswordField campoSenha = new JPasswordField();
+        campoSenha.setBounds(50, 180, 150, 30);
+        janela.add(campoSenha);
 
         JButton botaoLogar = new JButton("Login");
-        botaoLogar.setBounds(50, 230, 150, 30); // [00:20:49]
+        botaoLogar.setBounds(50, 230, 150, 30);
         janela.add(botaoLogar);
 
         botaoLogar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String usuario = campoUsuario.getText(); // [00:22:06]
-                String senha = new String(campoSenha.getPassword()); // [00:22:18] (recomendação de uso de getPassword() [00:22:43])
+                String usuario = campoUsuario.getText();
+                String senha = new String(campoSenha.getPassword());
 
-                System.out.printf("Você está fazendo o login com:\nUsuário: %s\nSenha: %s\n", usuario, senha); // [00:23:19]
+                System.out.printf("Você está fazendo o login com:\nUsuário: %s\nSenha: %s\n", usuario, senha);
 
-                campoUsuario.setText(""); // [00:24:28]
-                campoSenha.setText(""); // [00:24:40]
+                campoUsuario.setText("");
+                campoSenha.setText("");
             }
         });
     }
-}
